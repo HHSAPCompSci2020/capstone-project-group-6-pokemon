0da68f864a3a3dc60705dfb904e3661678ebd9f4
Rishi Zamvar <70030306+rzamvar@users.noreply.github.com>
2021-05-07 09:55:17 -0700

    ALPHA COMMIT

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -4,11 +4,16 @@ public class Pokemon {
 	int health, level;
 	boolean isAlive;
 	
-	String [] attackName = new String[3];
-	String [] attackValue = new String[3];
+	String [] attackName = new String[4];
+	int [] attackValue = new int[4];
 	
 	char type; // a = air, f = fire, e = earth, w = water;
 	
 	public Pokemon() {
+		type = 'n';
+		for (int i = 0; i < attackName.length; i++) {
+			attackName[i] = "protect";  
+		}
+		attackValue[1] = 0;
 	}
 }
