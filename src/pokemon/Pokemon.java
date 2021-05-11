c128a5df360d94cdd1e1c00807b65d5671ec8671
Rishi Zamvar <70030306+rzamvar@users.noreply.github.com>
2021-05-11 09:35:16 -0700

    worked on super class

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -8,13 +8,15 @@ public class Pokemon {
 	
 	char type; // a = air, f = fire, e = earth, w = water;
 	
-	int aStatN;  // attack stat numerator and denominator
+	int aStatN;  // attack stat numerator 
 	
 	double aStatM; // attack stat multiplier
 	
-	int sStatN;  // speed stat numerator and denominator
+	int sStatN;  // speed stat numerator 
 	
 	double sStatM; // speed stat multiplier
+	
+	public boolean isProtected; //for move protect
 	
 	
 	public Pokemon() {
@@ -22,6 +24,14 @@ public class Pokemon {
 		health = 100;
 		level = 50;
 		isAlive = true; 
+		
+		aStatN = 2;
+		aStatM = aStatN/2;
+		
+		sStatN = 2;
+		sStatM = sStatN/2;
+		
+		
 	}
 	
 	public Pokemon (char type, int health, int level) {
@@ -34,6 +44,36 @@ public class Pokemon {
 		
 		sStatN = 2;
 		sStatM = sStatN/2;
+	}
+	
+	public int getHealth() {
+		return health; 
+	}
+	
+	public char getType() {
+		return type; 
+	}
+	
+	public int getLevel() {
+		return level; 
+	}
+	
+	public void changeAstat(int amt) {
+		aStatN += amt; 
+		aStatM = aStatN/2;
+	}
+	
+	public void changeSstat(int amt) {
+		sStatN += amt; 
+		sStatM = sStatN/2;
+	}
+	
+	public double getAstat() {
+		return aStatM;
+	}
+	
+	public double getSstat() {
+		return sStatM;
 	}
 	
 	
