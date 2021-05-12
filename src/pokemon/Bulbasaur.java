e607c3e54be978a48aa8360f6704216e7e006d48
Rishi Zamvar <70030306+rzamvar@users.noreply.github.com>
2021-05-12 12:58:51 -0700

    changed bulbasaur

--- src/pokemon/Bulbasaur.java
+++ src/pokemon/Bulbasaur.java
@@ -2,12 +2,18 @@ package pokemon;
 
 public class Bulbasaur extends Pokemon {
 	
+	
+	
+	
+	
+	
 	public Bulbasaur() {
 		super();
 	}
 	
 	public Bulbasaur(int health, int level) {
 		super('g', health, level);
+		speedBase = 40;
 	}
 	
 	public int vineWhip () {
