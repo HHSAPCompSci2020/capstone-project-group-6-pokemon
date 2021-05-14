516f1299cebf0b718e71e3f4e2b5e6939d85fa8d
isa-pudiyapura <32914634+github210878@users.noreply.github.com>
2021-05-13 21:10:14 -0700

    Update BattleScreen.java

--- src/menus/BattleScreen.java
+++ src/menus/BattleScreen.java
@@ -13,13 +13,15 @@ public class BattleScreen extends Screen{
 	private DrawingSurface surface;
 
 	private Rectangle[] actions = new Rectangle[4];
-	private Rectangle[] statTag = new Rectangle[2];
+	private Rectangle[] stats = new Rectangle[2];
 	
 	private boolean[] clickState = new boolean[4];
 	
 	private Rectangle dialogue;
+	private Rectangle health1;
 	
 	private String[] actionLabels;
+	private String dialogueText;
 	
 	private boolean turn = false;
 	//when false, it is player 1's turn, when true it is player 2's turn
@@ -41,8 +43,9 @@ public class BattleScreen extends Screen{
 		}
 		
 		//stats for each pokemon
-		statTag[0] = new Rectangle(20, 400, 350, 120);
-		statTag[1] = new Rectangle(350, 100, 350, 75);
+		stats[0] = new Rectangle(20, 400, 350, 120);
+		stats[1] = new Rectangle(350, 100, 350, 75);
+		health1 = new Rectangle(370,120,200,10);
 		
 		//dialogue
 		dialogue = new Rectangle(50,540,700,50);
@@ -67,19 +70,30 @@ public class BattleScreen extends Screen{
 		//adding shapes
 		for(int i = 0; i<actions.length; i++)
 		{
+			if(clickState[i])
+			{
+				surface.fill(100);
+			}
 			surface.rect(actions[i].x, actions[i].y, actions[i].width, actions[i].height, 10, 10, 10, 10);
+			surface.noFill();
 		}
 		
-		for(int i = 0; i<statTag.length; i++)
+		for(int i = 0; i<stats.length; i++)
 		{
-			surface.rect(statTag[i].x, statTag[i].y, statTag[i].width, statTag[i].height, 10, 10, 10, 10);
+			surface.rect(stats[i].x, stats[i].y, stats[i].width, stats[i].height, 10, 10, 10, 10);
 		}
 		
 		surface.rect(dialogue.x, dialogue.y, dialogue.width, dialogue.height, 10, 10, 10, 10);
 		
+		surface.fill(255, 0, 0);
+		surface.rect(health1.x, health1.y, health1.width, health1.height);
+		
 		surface.fill(0);
 
+		
 		//adding text
+		
+		//action button labels
 		for (int i = 0; i < actions.length; i++) {
 			float w = surface.textWidth(actionLabels[i]);
 			surface.text(actionLabels[i], 
@@ -88,12 +102,30 @@ public class BattleScreen extends Screen{
 
 		}
 		
+		//dialogue box text
+		dialogueText = "dialogue";
+		float dialogueTextWidth = surface.textWidth(dialogueText);
+		surface.text(dialogueText, dialogue.x + dialogue.width / 2 - dialogueTextWidth / 2, dialogue.y + dialogue.height / 2);
+		
+		//stats
+		
+		
+		
+		
 		surface.popStyle();
 	}
 	
 	public void mousePressed()
 	{
 		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX, surface.mouseY));
+		
+		for(int i = 0; i<clickState.length; i++)
+		{
+			if(actions[i].contains(p))
+			{
+				clickState[i] = !clickState[i];
+			}
+		}
 	}
 
 }
